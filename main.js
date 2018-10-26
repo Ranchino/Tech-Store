99d2c68254a21d0c2bd90faa8b7690dc999af4ab
Ranchino Bahadin <ranjbahadin@live.se>
2018-10-26 15:15:38 +0200

    init project

---
+++ main.js
@@ -0,0 +1,31 @@
+var listOfProducts;
+
+/** Get products from the json file and store it in a gobal variable */
+function loadProducts() {
+    fetch("./products.json")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(products) {
+        listOfProducts = products;
+        addProductsToWebpage();
+    });
+}
+
+
+function initSite() {
+    loadProducts();
+    // This would also be a good place to initialize other parts of the UI
+}
+
+/** Uses the loaded products data to create a visible product list on the website */
+function addProductsToWebpage() {
+    // Check your console to see that the products are stored in the listOfProducts varible.
+    console.log(listOfProducts);
+
+    // Add your code here, remember to brake your code in to smaller function blocks
+    // to reduce complexity and increase readability. Each function should have
+    // an explainetory comment like the one for this function, see row 22.
+    
+    // TODO: Remove the console.log and these comments when you've read them.
+}
