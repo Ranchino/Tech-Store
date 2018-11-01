70e32bee41bf196cd1e05bee2e4cb52006fa9bbc
David Yan <daveddd97@hotmail.com>
2018-11-01 21:58:26 +0100

    home page front end complete

--- main.js
+++ main.js
@@ -1,32 +1,56 @@
-var listOfProducts;
-// TEEEETasdsadfsdfsdf
-
-/** Get products from the json file and store it in a gobal variable "*/
-function loadProducts() {
+/* Getting json files ready */
     fetch("./products.json")
     .then(function(response) {
         return response.json();
     })
-    .then(function(products) {
-        listOfProducts = products;
+    .then(function(data) {
+        listOfProducts = data.phones;
         addProductsToWebpage();
     });
-}
 
+/* Creating one parent div for all */
+var wrapperForAllPhones = document.createElement("div")
+wrapperForAllPhones.className = "wrapperForAllPhones"
 
-function initSite() {
-    loadProducts();
-    // This would also be a good place to initialize other parts of the UI
-}
-
-/** Uses the loaded products data to create a visible product list on the website */
+/** Not sure what this does */
 function addProductsToWebpage() {
-    // Check your console to see that the products are stored in the listOfProducts varible.
-    console.log(listOfProducts);
+    for(var index = 0; index < listOfProducts.length; index++)
+    {
+        var phone = createPhoneCard(listOfProducts[index])
+        wrapperForAllPhones.appendChild(phone)
+    }
+    document.body.appendChild(wrapperForAllPhones)
+}
+  
+ /* This makes json objects visual on website */
+function createPhoneCard(listOfProducts) {
+    var phone = document.createElement("div")
+    phone.className = "phoneCardClass"
 
-    // Add your code here, remember to brake your code in to smaller function blocks
-    // to reduce complexity and increase readability. Each function should have
-    // an explainetory comment like the one for this function, see row 22.
-    
-    // TODO: Remove the console.log and these comments when you've read them.
+    var getPhoneName = document.createElement("h1")
+    getPhoneName.innerText = listOfProducts.title
+    phone.appendChild(getPhoneName)
+
+    var getDescription = document.createElement("h5")
+    getDescription.innerText = listOfProducts.description
+    phone.appendChild(getDescription)
+
+    var getPhoneImage = document.createElement("img")
+    getPhoneImage.src = "/assets/" + listOfProducts.image
+    phone.appendChild(getPhoneImage)
+
+    var getPhonePrice = document.createElement("h3")
+    getPhonePrice.innerText = listOfProducts.price + " kr"
+    phone.appendChild(getPhonePrice)
+
+    var getPhonePrice = document.createElement("a")
+    getPhonePrice.className = "add-to-cart fas fa-cart-arrow-down"
+    getPhonePrice.setAttribute('href', "#läggtill")
+    getPhonePrice.innerText = " Lägg till i kundvagnen"
+
+    phone.appendChild(getPhonePrice)
+
+   
+
+    return phone
 }
