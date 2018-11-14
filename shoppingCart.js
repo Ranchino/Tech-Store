9a087b7d473430916ebb95b6638620e854af8cf4
Peter Cavar <33002520+PCavar@users.noreply.github.com>
2018-11-14 13:27:59 +0100

    improvement on fixing deletePhone()

--- shoppingCart.js
+++ shoppingCart.js
@@ -63,7 +63,7 @@ function initSite() {
 }
 
 /* here is a printProductsInCart function that I call in i initSite to count the sum of phones added */
-function printProductsInCart(cartItem) {
+function printProductsInCart() {
     
     var phoneArray = JSON.parse(localStorage.shoppingCart);
 
@@ -87,11 +87,9 @@ function printProductsInCart(cartItem) {
 /* deletProducts form cart page */
 function deletePhone(cartItem) {
     
-    var phoneArray = JSON.parse(shoppingCart);
+    var phoneArray = JSON.parse(localStorage.shoppingCart);
     localStorage.shoppingCart = phoneArray;
 
-
-    console.log(phoneArray)
     if (localStorage.clickcount) {
         localStorage.clickcount = Number(localStorage.clickcount) - 1;
         document.querySelector(".number-of-orders").innerHTML = localStorage.clickcount;
@@ -101,11 +99,12 @@ function deletePhone(cartItem) {
     for (var i = 0; i < phoneArray.length; i++) {
         
         if (cartItem.dateOfClick == phoneArray[i].dateOfClick) {
-            phoneArray.splice(i, 1)
+            phoneArray[i].dateOfClick.splice(i, 1)
             break;
         }
-        console.log(phoneArray)
     }
+    var phoneArray = JSON.stringify(localStorage.shoppingCart);
+    localStorage.shoppingCart = phoneArray;
     printProductsInCart();
 }
 
